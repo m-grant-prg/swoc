f8eadf4049c332914dcd70f1ddd9e851e57e2aed
Mark Grant <m.grant.prg@gmail.com>
2018-01-12 15:23:42 +0000

    Initial commit.
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

---
+++ source/progs/c/src/swocserverd/cmdlineargs.c
@@ -0,0 +1,110 @@
+/**
+ * @file srv-prg/source/progs/c/src/swocserverd/cmdlineargs.c
+ *
+ * Command line argument processing for swocserverd using getopt_long.
+ *
+ * @author Copyright (C) 2016-2017  Mark Grant
+ *
+ * Released under the GPLv3 only.\n
+ * SPDX-License-Identifier: GPL-3.0
+ *
+ * @version _v1.0.3 ==== 18/11/2017_
+ */
+
+/* **********************************************************************
+ *									*
+ * Changelog								*
+ *									*
+ * Date		Author	Version	Description				*
+ *									*
+ * 19/11/2016	MG	1.0.1	First release.				*
+ * 04/06/2017	MG	1.0.2	Tidy up unnecessary include statements.	*
+ * 18/11/2017	MG	1.0.3	Add Doxygen comments.			*
+ *				Add SPDX license tag.			*
+ *									*
+ ************************************************************************
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sysexits.h>
+#include <getopt.h>
+
+#include <swocserverd.h>
+#include <cmdlineargs.h>
+
+
+/**
+ * Process command line arguments using getopt_long.
+ * Variable length argument list comprising of the flag structs. In this program
+ * there are none.
+ * @param argc The standard CLA argc.
+ * @param argv The standard CLA argv.
+ * @return 0 on success, on failure standard EX_USAGE (64) command line  usage
+ * error.
+ */
+int process_cla(int argc, char **argv, ...)
+{
+	/* getopt_long stores the option index here. */
+	int option_index = 0;
+	int c;
+
+	struct option long_options[] = {
+		{"debug",		no_argument,		0,	'D'},
+		{"help",		no_argument,		0,	'h'},
+		{"version",		no_argument,		0,	'V'},
+		{0,			0,			0,	0}
+	};
+
+	while ((c = getopt_long(argc, argv, "DhV",
+		long_options, &option_index)) != -1) {
+
+		switch (c) {
+		case 'D':
+			debug = 1;
+			break;
+
+		case 'h':
+			printf("%s %s", argv[0], " - Help option.\n");
+			printf("\tLong and short options can be mixed on the "
+				"command line but if an option\ntakes an "
+				"optional argument it is best to enter "
+				"-o\"argument\" or\n--option=argument.\n");
+			printf("-D | --debug\tDon't daemonise. Output "
+				"information to stdout and stderr.\n");
+			printf("-V | --version\tDisplay version "
+				"information.\n");
+			if (!swsd_err)
+				swsd_err = -1;
+			break;
+
+		case 'V':
+			printf("%s %s %s %s", argv[0], "Source version -",
+				swocserverd_get_src_version(), "\n");
+			printf("%s %s %s %s", argv[0], "Package version -",
+				swocserverd_get_pkg_version(), "\n");
+			if (!swsd_err)
+				swsd_err = -1;
+			break;
+
+		case '?':
+			/* getopt_long already printed a message. */
+			swsd_err = EX_USAGE;
+			break;
+
+		default:
+			abort();
+		}
+	}
+
+	/* Non-option arguments are not accepted. */
+	if (optind < argc) {
+		fprintf(stderr, "Program does not accept other arguments.\n");
+		swsd_err = EX_USAGE;
+	}
+	if (swsd_err == -1) {
+		swsd_err = 0;
+		return 1;
+	}
+	return swsd_err;
+}
---
+++ source/progs/c/src/swocserverd/request.c
@@ -0,0 +1,336 @@
+/**
+ * @file srv-prg/source/progs/c/src/swocserverd/request.c
+ *
+ * Request processing functions.
+ *
+ * @author Copyright (C) 2016-2017  Mark Grant
+ *
+ * Released under the GPLv3 only.\n
+ * SPDX-License-Identifier: GPL-3.0
+ *
+ * @version _v1.0.9 ==== 18/11/2017_
+ */
+
+/* **********************************************************************
+ *									*
+ * Changelog								*
+ *									*
+ * Date		Author	Version	Description				*
+ *									*
+ * 19/11/2016	MG	1.0.1	First release.				*
+ * 08/01/2017	MG	1.0.2	Add creation / deletion of pid file.	*
+ *				(Recommended for a systemd service of	*
+ *				type forking.)				*
+ * 02/02/2017	MG	1.0.3	Migrate to use of mge_errno in libmgec.	*
+ * 13/02/2017	MG	1.0.4	Implement config file reload		*
+ *				functionality.				*
+ * 22/04/2017	MG	1.0.5	Change to use new bstree struct.	*
+ * 27/05/2017	MG	1.0.6	Give daemon its own config file.	*
+ * 				Use new local validateconfig.		*
+ * 				Add support for temporary include	*
+ *				directory.				*
+ * 04/06/2017	MG	1.0.7	Split comms functions out of main into	*
+ *				their own source file.			*
+ *				Tidy up unnecessary include statements.	*
+ * 				Use more meaningful name for client	*
+ *				lock bstree.				*
+ * 07/06/2017	MG	1.0.8	Implement epoll controlled use of	*
+ *				multiple ports.				*
+ * 14/09/2017	MG		Change force_unlock to unlock.		*
+ * 18/11/2017	MG	1.0.9	Add Doxygen comments.			*
+ *				Add SPDX license tag.			*
+ *									*
+ ************************************************************************
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <syslog.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <mgememory.h>
+#include <mge-errno.h>
+#include <swocserverd.h>
+#include <bstree.h>
+
+
+/**
+ * swocserver requesting the daemon to terminate.
+ * @param mgemessage The message being processed.
+ * @param msg_args The message arguments.
+ * @return 0 on success, non-zero on failure.
+ */
+int end_req(struct mgemessage *msg, enum msg_arguments *msg_args)
+{
+	char outgoing_msg[100];
+	swsd_err = 0;
+
+	if (msg->argc > 2) {
+		*msg_args = args_err;
+		return swsd_err;
+	}
+	end = 1;
+
+	if (debug)
+		printf("Termination request received - exiting.\n");
+	syslog((int) (LOG_USER | LOG_NOTICE), "Termination request received - "
+		"exiting.");
+
+	sprintf(outgoing_msg, "swocserverd,end,ok;");
+	send_outgoing_msg(outgoing_msg, strlen(outgoing_msg), &cursockfd);
+	swsd_err = mge_errno;
+	return swsd_err;
+}
+
+/**
+ * swocserver request to reload config file.
+ * @param mgemessage The message being processed.
+ * @param msg_args The message arguments.
+ * @return 0 on success, non-zero on failure.
+ */
+int reload_req(struct mgemessage *msg, enum msg_arguments *msg_args)
+{
+	char outgoing_msg[100];
+	swsd_err = 0;
+
+	if (msg->argc > 2) {
+		*msg_args = args_err;
+		return swsd_err;
+	}
+
+	swsd_err = swsd_validate_config();
+	if (swsd_err) {
+		if (debug)
+			fprintf(stderr, "Validate config errored with %i.\n",
+				mge_errno);
+		syslog((int) (LOG_USER | LOG_NOTICE), "Validate config errored "
+			"with %i.", mge_errno);
+		sprintf(outgoing_msg, "swocserverd,reload,err,%i;", mge_errno);
+		send_outgoing_msg(outgoing_msg, strlen(outgoing_msg),
+			&cursockfd);
+		exit(swsd_err);
+	}
+	if (debug)
+		printf("Config file reloaded.\n");
+	syslog((int) (LOG_USER | LOG_NOTICE), "Config file reloaded.");
+	sprintf(outgoing_msg, "swocserverd,reload,ok;");
+	send_outgoing_msg(outgoing_msg, strlen(outgoing_msg), &cursockfd);
+	swsd_err = mge_errno;
+	return swsd_err;
+}
+
+/**
+ * Server status request.
+ * @param mgemessage The message being processed.
+ * @param msg_args The message arguments.
+ * @return 0 on success, non-zero on failure.
+ */
+int server_status_req(struct mgemessage *msg, enum msg_arguments *msg_args)
+{
+	char *outgoing_msg, *t_outgoing_msg = NULL;
+	size_t outgoing_msg_size = DEF_MSG_SIZE;
+	char tmp_msg[_POSIX_HOST_NAME_MAX + 100] = "";
+	char *client_lu = "";
+	int counter;
+	swsd_err = 0;
+
+	if (msg->argc > 2) {
+		*msg_args = args_err;
+		return swsd_err;
+	}
+
+	if ((int) outgoing_msg_size < 23)
+		outgoing_msg_size = 23;
+	outgoing_msg = mg_realloc(NULL, outgoing_msg_size);
+	if (outgoing_msg == NULL) {
+		swsd_err = errno;
+		return swsd_err;
+	}
+	*outgoing_msg = '\0';
+	strcat(outgoing_msg, "swocserverd,status,ok");
+
+	while((client_lu = (char *) find_next_bst_node(locks, client_lu))
+		!= NULL) {
+
+		counter = get_counter_bst_node(locks, client_lu);
+
+		sprintf(tmp_msg, ",%s,%i", client_lu, counter);
+		if (((int) outgoing_msg_size - (int) strlen(outgoing_msg))
+				< ((int) strlen(tmp_msg) + 2)) {
+			outgoing_msg_size += (int) strlen(tmp_msg) + 2;
+			t_outgoing_msg = outgoing_msg;
+			outgoing_msg = mg_realloc(t_outgoing_msg,
+							outgoing_msg_size);
+			if (outgoing_msg == NULL) {
+				swsd_err = errno;
+				free(t_outgoing_msg);
+				return swsd_err;
+			}
+		}
+
+		if (debug)
+			printf("Client %s has %i locks.\n", client_lu, counter);
+		strcat(outgoing_msg, tmp_msg);
+	}
+	strcat(outgoing_msg, ";");
+	send_outgoing_msg(outgoing_msg, strlen(outgoing_msg), &cursockfd);
+	swsd_err = mge_errno;
+
+	free(outgoing_msg);
+	return swsd_err;
+}
+
+/**
+ * Server lock removal.
+ * Exactly 1 argument, the client lock to be removed.
+ * @param mgemessage The message being processed.
+ * @param msg_args The message arguments.
+ * @return 0 on success, non-zero on failure.
+ */
+int unlock_req(struct mgemessage *msg, enum msg_arguments *msg_args)
+{
+	struct bstree *plocks;
+	char outgoing_msg[100];
+	swsd_err = 0;
+
+	if (msg->argc != 3) {
+		*msg_args = args_err;
+		return swsd_err;
+	}
+	plocks = del_bst_node(locks, *(msg->argv + 2));
+
+	if (plocks == NULL) {
+		if (debug)
+			fprintf(stderr, "Node removal errored with %i.\n",
+				mge_errno);
+		syslog((int) (LOG_USER | LOG_NOTICE), "Node removal errored "
+			"with %i.", mge_errno);
+		sprintf(outgoing_msg, "swocserverd,unlock,err,%i;", mge_errno);
+	} else {
+		locks = plocks;
+		if (debug)
+			printf("Lock %s removed by server.\n",
+				*(msg->argv + 2));
+		syslog((int) (LOG_USER | LOG_NOTICE), "Client %s lock removed "
+			"by server.", *(msg->argv + 2));
+		sprintf(outgoing_msg, "swocserverd,unlock,ok,%s;",
+			*(msg->argv + 2));
+	}
+	send_outgoing_msg(outgoing_msg, strlen(outgoing_msg), &cursockfd);
+	swsd_err = mge_errno;
+	return swsd_err;
+}
+
+/**
+ * Client lock request.
+ * No parameters allowed, add client lock.
+ * @param mgemessage The message being processed.
+ * @param msg_args The message arguments.
+ * @return 0 on success, non-zero on failure.
+ */
+int lock_req(struct mgemessage *msg, enum msg_arguments *msg_args)
+{
+	struct bstree *plocks;
+	char outgoing_msg[100];
+	swsd_err = 0;
+
+	if (msg->argc > 2) {
+		*msg_args = args_err;
+		return swsd_err;
+	}
+	plocks = add_bst_node(locks, client, strlen(client) + 1);
+
+	if (plocks == NULL) {
+		if (debug)
+			fprintf(stderr, "Node add errored with %i.\n",
+				mge_errno);
+		syslog((int) (LOG_USER | LOG_NOTICE), "Node add errored "
+			"with %i.", mge_errno);
+		sprintf(outgoing_msg, "swocserverd,lock,err,%i;", mge_errno);
+	} else {
+		locks = plocks;
+		if (debug)
+			printf("Lock %s added.\n", client);
+		syslog((int) (LOG_USER | LOG_NOTICE), "Client %s lock added.",
+			client);
+		sprintf(outgoing_msg, "swocserverd,lock,ok;");
+	}
+	send_outgoing_msg(outgoing_msg, strlen(outgoing_msg), &cursockfd);
+	swsd_err = mge_errno;
+	return swsd_err;
+}
+
+/**
+ * Release request from client.
+ * @param mgemessage The message being processed.
+ * @param msg_args The message arguments.
+ * @return 0 on success, non-zero on failure.
+ */
+int release_req(struct mgemessage *msg, enum msg_arguments *msg_args)
+{
+	struct bstree *plocks;
+	char outgoing_msg[100];
+	swsd_err = 0;
+
+	if (msg->argc > 2) {
+		*msg_args = args_err;
+		return swsd_err;
+	}
+	plocks = del_bst_node(locks, client);
+
+	if (plocks == NULL) {
+		if (debug)
+			fprintf(stderr, "Node delete errored with %i.\n",
+				mge_errno);
+		syslog((int) (LOG_USER | LOG_NOTICE), "Node delete errored "
+			"with %i.", mge_errno);
+		sprintf(outgoing_msg, "swocserverd,release,err,%i;", mge_errno);
+	} else {
+		locks = plocks;
+		if (debug)
+			printf("Lock %s removed.\n", client);
+		syslog((int) (LOG_USER | LOG_NOTICE), "Client %s lock removed.",
+			client);
+		sprintf(outgoing_msg, "swocserverd,release,ok;");
+	}
+	send_outgoing_msg(outgoing_msg, strlen(outgoing_msg), &cursockfd);
+	swsd_err = mge_errno;
+	return swsd_err;
+}
+
+/**
+ * Status request from client.
+ * @param mgemessage The message being processed.
+ * @param msg_args The message arguments.
+ * @return 0 on success, non-zero on failure.
+ */
+int client_status_req(struct mgemessage *msg, enum msg_arguments *msg_args)
+{
+	char outgoing_msg[100];
+	swsd_err = 0;
+	int counter = 0;
+
+	if (msg->argc > 2) {
+		*msg_args = args_err;
+		return swsd_err;
+	}
+	counter = get_counter_bst_node(locks, client);
+
+	if (counter == -1) {
+		if (debug)
+			fprintf(stderr, "Node count errored with %i.\n",
+				mge_errno);
+		syslog((int) (LOG_USER | LOG_NOTICE), "Node count errored "
+			"with %i.", mge_errno);
+		sprintf(outgoing_msg, "swocserverd,status,err,%i;", mge_errno);
+	} else {
+		if (debug)
+			printf("Client %s has %i locks.\n", client, counter);
+		sprintf(outgoing_msg, "swocserverd,status,ok,%i;", counter);
+	}
+	send_outgoing_msg(outgoing_msg, strlen(outgoing_msg), &cursockfd);
+	swsd_err = mge_errno;
+	return swsd_err;
+}
