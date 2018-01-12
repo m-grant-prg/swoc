4e305054d6b94a5874b1953a3c6f2db9f0a49f65
Mark Grant <m.grant.prg@gmail.com>
2018-01-12 15:23:42 +0000

    Initial commit.
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

---
+++ source/progs/c/src/test-messages/main.c
@@ -0,0 +1,52 @@
+/* **********************************************************************
+ *									*
+ * Source: main.c							*
+ * Author: Mark Grant							*
+ *									*
+ * Purpose:								*
+ *	Test program for messages in libswoccommon shared library.	*
+ * 									*
+ * Released under the GPLv3 only.					*
+ * SPDX-License-Identifier: GPL-3.0					*
+ *									*
+ ************************************************************************
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <mge-errno.h>
+#include <libswoccommon.h>
+
+
+/*
+ * messages test program.
+ */
+int main(int argc, char** argv)
+{
+	int prog_error = 0;
+	char *outgoing_msg = "swocserver,status;";
+	size_t om_length = strlen(outgoing_msg);
+	struct mgemessage msg1 = { NULL, 0, 0, 0, ';', ',', 0, NULL };
+	struct mgemessage *msg = &msg1;
+
+	pollint = 10;
+	ssh = 0;
+	strcpy(server, "localhost");
+	srvportno = 62522;
+	sshportno = 62521;
+	strcpy(sshuser, "swoc");
+
+	printf("exch_msg.\n");
+	prog_error = exch_msg(outgoing_msg, om_length, msg);
+
+	printf("prog_error is %i\n", prog_error);
+	if (prog_error)
+		fprintf(stderr, "%s\n", mge_strerror(mge_errno));
+	else
+		printf("msg->message is %s\n", msg->message);
+	clear_msg(msg, ';', ',');
+
+	exit(prog_error);
+}
---
+++ source/progs/c/src/test-validate/testvalidate.c
@@ -0,0 +1,46 @@
+/* **********************************************************************
+ *									*
+ * Source: testvalidate.c						*
+ * Author: Mark Grant							*
+ *									*
+ * Purpose:								*
+ *	To test the validate config functions.				*
+ *									*
+ * Released under the GPLv3 only.					*
+ * SPDX-License-Identifier: GPL-3.0					*
+ *									*
+ ************************************************************************
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libswoccommon.h>
+
+
+/*
+ * Main
+ */
+int main(int argc, char **argv)
+{
+	int retval = 0;
+
+	retval = swcom_validate_config();
+
+	printf("retval after validate is %i\n", retval);
+
+	printf("pollint is:- %i\n", pollint);
+
+	printf("SSH is:- %i\n", ssh);
+
+	printf("server name is:- %s\n", server);
+
+	printf("srvportno is:- %i\n", srvportno);
+
+	printf("sshportno is:- %i\n", sshportno);
+
+	printf("user name is:- %s\n", sshuser);
+
+	exit(retval);
+}
