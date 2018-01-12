2ab082b5393498ec0997566ddcd46f501f21ae69
Mark Grant <m.grant.prg@gmail.com>
2018-01-12 15:23:42 +0000

    Initial commit.
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

---
+++ source/progs/c/inc/libswocclient.h
@@ -0,0 +1,61 @@
+/**
+ * @file cli-dev/source/progs/c/inc/libswocclient.h
+ *
+ * Header file for Server Wait on Clients client-side library.
+ *
+ * @author Copyright (C) 2016-2018  Mark Grant
+ *
+ * Released under the GPLv3 only.\n
+ * SPDX-License-Identifier: GPL-3.0
+ *
+ * @version _v1.0.6 ==== 11/11/2017_
+ */
+
+/* **********************************************************************
+ *									*
+ * Changelog								*
+ *									*
+ * Date		Author	Version	Description				*
+ *									*
+ * 16/07/2016	MG	1.0.1	First version release.			*
+ * 17/07/2016	MG	1.0.2	Move towards kernel coding style.	*
+ * 06/01/2017	MG	1.0.3	Modify to use swocserverd daemon and	*
+ *				TCP stream.				*
+ * 06/02/2017	MG	1.0.4	Add pollint and wait function.		*
+ * 02/10/2017	MG	1.0.5	Move validate config to common.		*
+ * 11/11/2017	MG	1.0.6	Add Doxygen comments.			*
+ *				Add SPDX license tag.			*
+ *									*
+ ************************************************************************
+ */
+
+
+#ifndef LIBSWOCCLIENT_H
+#define LIBSWOCCLIENT_H
+
+
+#include <portability.h>
+
+
+BEGIN_C_DECLS
+
+
+int swc_show_status(void);
+
+int swc_set_lock(void);
+
+int swc_rel_lock(void);
+
+int swc_client_wait(void);
+
+char *libswocclient_get_pkg_version(void);
+
+char *libswocclient_get_src_version(void);
+
+void libswocclient_print_pkg_version(void);
+
+void libswocclient_print_src_version(void);
+
+END_C_DECLS
+
+#endif /* ndef LIBSWOCCLIENT_H */
