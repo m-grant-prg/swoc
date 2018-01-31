ab1d9922deb8bc61865da6da8f30d4ac9a66f8c4
Mark Grant <m.grant.prg@gmail.com>
2018-01-31 15:30:28 +0000

    cli-lib: Add release with no locks test.
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

--- src/prg/c/src/test-optionproc/testoptionproc.c
+++ src/prg/c/src/test-optionproc/testoptionproc.c
@@ -50,6 +50,10 @@ int main(int argc, char **argv)
 	prg_err = swc_client_wait();
 	printf("prg_err after swc_client_wait with 2 locks is %i\n", prg_err);
 
+	swc_show_status();
+	prg_err = swc_rel_lock();
+	printf("prg_err after swc_rel_lock is %i\n", prg_err);
+
 	libswocclient_print_pkg_version();
 	libswocclient_print_src_version();
 
