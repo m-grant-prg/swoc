6daca887342aae95203efc7bf2bf0b85ee373297
Mark Grant <m.grant.prg@gmail.com>
2018-06-05 10:25:12 +0100

    srv-lib: Make version.c ChangeLog for that file only.
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

--- srv-lib/src/prg/c/src/libswocserver/version.c
+++ srv-lib/src/prg/c/src/libswocserver/version.c
@@ -3,12 +3,12 @@
  *
  * Source for version functions.
  *
- * @author Copyright (C) 2016-2017  Mark Grant
+ * @author Copyright (C) 2016-2018  Mark Grant
  *
  * Released under the GPLv3 only.\n
  * SPDX-License-Identifier: GPL-3.0
  *
- * @version _v1.1.3 ==== 12/11/2017_
+ * @version _v1.0.5 ==== 05/06/2018_
  */
 
 /* **********************************************************************
@@ -17,24 +17,12 @@
  *									*
  * Date		Author	Version	Description				*
  *									*
- * 25/05/2016	MG	1.0.1	First release.Separated out from	*
- *				swocserver-c package.			*
- * 12/06/2016	MG	1.0.2	Make publicly visible function names	*
- *				more unique.				*
- * 16/07/2016	MG	1.0.3	Move towards kernel coding style.	*
- * 17/07/2016	MG	1.0.4	Further coding style changes.		*
- * 06/05/2017	MG	1.1.0	Convert from NFS 'file as a flag'	*
- *				semaphore to TCP socket messaging to	*
- *				new server daemon.			*
- *				Migrate to use of mge_errno error	*
- *				handling from libmgec.			*
- *				Print the number of clients with locks	*
- *				in sws_show_status().			*
- * 12/09/2017	MG	1.1.1	Change sws_force_unlock() to		*
- *				sws_unlock().				*
- * 15/09/2017	MG	1.1.2	Change refernces to ssl to tls.		*
- * 12/11/2017	MG	1.1.3	Add Doxygen comments.			*
+ * 25/05/2016	MG	1.0.1	First release.				*
+ * 16/07/2016	MG	1.0.2	Move towards kernel coding style.	*
+ * 17/07/2016	MG	1.0.3	Further coding style changes.		*
+ * 12/11/2017	MG	1.0.4	Add Doxygen comments.			*
  *				Add SPDX license tags.			*
+ * 05/06/2018	MG	1.0.5	Make this ChangeLog for this file only.	*
  *									*
  ************************************************************************
  */
