5a6806bddd40592703969a1fe7511dd00d908ffd
Mark Grant <m.grant.prg@gmail.com>
2018-06-05 09:17:11 +0100

    cli-prg: Make version.c ChangeLog for that file only.
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

--- cli-prg/src/prg/c/src/version.c
+++ cli-prg/src/prg/c/src/version.c
@@ -8,7 +8,7 @@
  * Released under the GPLv3 only.\n
  * SPDX-License-Identifier: GPL-3.0
  *
- * @version _v1.1.3 ==== 22/05/2018_
+ * @version _v1.0.6 ==== 05/06/2018_
  */
 
 /* **********************************************************************
@@ -18,26 +18,13 @@
  * Date		Author	Version	Description				*
  *									*
  * 08/07/2015	MG	1.0.1	First release.				*
- * 08/07/2015	MG	1.0.2	Extracted remsyslog.h to a seperate	*
- *				libremsyslog-dev package.		*
- * 10/07/2015	MG	1.0.3	remsyslog extracted to libremsyslog	*
- *				library package.			*
- * 09/05/2016	MG	1.0.4	Move header files to include directory.	*
- * 11/06/2016	MG	1.0.5	Modified following introduction of	*
- *				libswocclient library.			*
- * 13/06/2016	MG	1.0.6	Adopt convention of using void in empty	*
+ * 13/06/2016	MG	1.0.2	Adopt convention of using void in empty	*
  *				function parameter lists.		*
- * 17/07/2016	MG	1.0.7	Move towards kernel coding style.	*
- * 07/05/2017	MG	1.1.0	Migrate to use mge_errno and handling	*
- *				from libmgec. Add error message output	*
- *				to prevent silence on error.		*
- *				Add --wait as a command line argument.	*
- * 12/11/2017	MG	1.1.1	Add Doxygen comments.			*
+ * 17/07/2016	MG	1.0.3	Move towards kernel coding style.	*
+ * 12/11/2017	MG	1.0.4	Add Doxygen comments.			*
  *				Add SPDX license tags.			*
- * 28/03/2018	MG	1.1.2	Add swocclient.h, (fixes sparse		*
- *				warnings).				*
- * 22/05/2018	MG	1.1.3	Simplify src directory structure and	*
- *				header file location.			*
+ * 22/05/2018	MG	1.0.5	Simplify src directory structure.	*
+ * 05/06/2018	MG	1.0.6	Make this ChangeLog for this file only.	*
  *									*
  ************************************************************************
  */
