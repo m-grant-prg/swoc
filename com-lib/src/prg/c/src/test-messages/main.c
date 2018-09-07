b1a61cb1fad00ce08529b21d2a51d99da20cd95e
Mark Grant <m.grant.prg@gmail.com>
2018-09-07 14:41:59 +0100

    com-lib: Use new mgemessage struct initialiser
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

--- com-lib/src/prg/c/src/test-messages/main.c
+++ com-lib/src/prg/c/src/test-messages/main.c
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
 	int prog_error = 0;
 	char *outgoing_msg = "swocserver,status;";
 	size_t om_length = strlen(outgoing_msg);
-	struct mgemessage msg1 = { NULL, 0, 0, 0, ';', ',', 0, NULL };
+	struct mgemessage msg1 = MGEMESSAGE_INIT(';', ',');
 	struct mgemessage *msg = &msg1;
 
 	pollint = 10;
