ca012f1ec272ae3f44b97f227dbaf47d1610dde0
Mark Grant <m.grant.prg@gmail.com>
2024-09-16 15:43:59 +0100

    srv-prg: Daemon crashes when client releases a non-existent lock
    The daemon crashes when client or server tries to release a lock
    when none are held.
    
    swocclient:-
    mgrantprg@horace:$ ./swocclient -r
    swocclient failed with error - No such lock.
    mgrantprg@horace:$
    
    swocserver:-
    mgrantprg@horace:$ ./swocserver -rlocalhost
    swocserver failed with error - No such lock.
    mgrantprg@horace:$
    
    The daemon exits after logging:-
    swocclient:-
    2024-09-16T10:13:40.788754+01:00 horace swocserverd: Host and IP matched - localhost 127.0.0.1
    2024-09-16T10:13:40.788991+01:00 horace swocserverd: Node delete errored with 6.
    2024-09-16T10:13:40.789244+01:00 horace swocclient: No such lock.
    
    swocserver:-
    2024-09-16T10:14:36.476942+01:00 horace swocserverd: Host and IP matched - localhost 127.0.0.1
    2024-09-16T10:14:36.491136+01:00 horace swocserverd: Node removal errored with 6.
    2024-09-16T10:14:36.491284+01:00 horace swocserver: Invalid message - swocserverd,release,err,11;
    
    If the daemon does not find a node to delete then it should just
    continue albeit returning an error message to swocclient or swocserver
    as it currently does.
    
    GitHub: Fixes #11
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

--- src/prg/c/src/srv-prg/swocserverd/request.c
+++ src/prg/c/src/srv-prg/swocserverd/request.c
@@ -8,7 +8,7 @@
  * Released under the GPLv3 only.\n
  * SPDX-License-Identifier: GPL-3.0-only
  *
- * @version _v1.1.1 ==== 26/05/2024_
+ * @version _v1.1.2 ==== 16/09/2024_
  */
 
 #include <arpa/inet.h>
@@ -287,6 +287,8 @@ int srv_cli_rel_req(struct mgemessage *msg, enum msg_arguments *msg_args)
 		snprintf(out_msg, ARRAY_SIZE(out_msg),
 			 "swocserverd,release,err,%i;", mge_errno);
 		send_outgoing_msg(out_msg, strlen(out_msg), &cursockfd);
+		if (mge_errno == MGE_LOCK_NOT_FOUND)
+			return 0;
 		return -mge_errno;
 	}
 
@@ -685,6 +687,8 @@ int cli_rel_req(struct mgemessage *msg, enum msg_arguments *msg_args)
 		snprintf(out_msg, ARRAY_SIZE(out_msg),
 			 "swocserverd,release,err,%i;", mge_errno);
 		send_outgoing_msg(out_msg, strlen(out_msg), &cursockfd);
+		if (mge_errno == MGE_LOCK_NOT_FOUND)
+			return 0;
 		return -mge_errno;
 	}
 	cli_locks = plocks;
